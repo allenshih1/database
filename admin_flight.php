65daf252fff02825bd57bfc5fa70b54be375f972
Yi-Shin Shih <yihshih@cs.nctu.edu.tw>
2014-04-06 20:45:31 +0800

    take apart update_flight_function

--- admin_flight.php
+++ admin_flight.php
@@ -3,47 +3,6 @@
 if(isset($_SESSION['isAuth']) && $_SESSION['isAdmin'])
 {
   require_once("db.php");
-  if(isset($_POST['update_flight']))
-  {
-    $uf = false;
-    if(preg_match('/ /', $_POST['flight_number']) || $_POST['flight_number'] ===''){
-      echo "flight_number cannot contain space or be empty<br>";
-      $uf = true;
-    }
-    if(preg_match('/ /', $_POST['departure']) || $_POST['departure'] ===''){
-      echo "depature cannot contain space or be empty<br>";
-      $uf = true;
-    }
-    if(preg_match('/ /', $_POST['destination']) || $_POST['destination'] ===''){
-      echo "destination cannot contain space or be empty<br>";
-      $uf = true;
-    }
-    if($_POST['departure_date'] ===''){
-      echo "depature_date cannot be empty<br>";
-      $uf = true;
-    }
-    if($_POST['arrival_date'] ===''){
-      echo "arrival_date cannot be empty<br>";
-      $uf = true;
-    }
-    if($uf !== true)
-    {
-    $sql = "UPDATE Flight SET flight_number = ?,"
-      ." departure = ?,"
-      ." destination = ?,"
-      ." departure_date = ?,"
-      ." arrival_date = ?"
-      ." WHERE ID = ?";
-    $update_flight = $db->prepare($sql);
-    $update_flight->execute(
-      array($_POST['flight_number'],
-      $_POST['departure'],
-      $_POST['destination'],
-      $_POST['departure_date'],
-      $_POST['arrival_date'],
-      $_POST['id']));
-    }
-  }
   $sql = "SELECT * FROM Flight";
   $flights = $db->prepare($sql);
   $flights->execute();
@@ -65,7 +24,7 @@ if(isset($_SESSION['isAuth']) && $_SESSION['isAdmin'])
     <?
     if(isset($_POST['update']) && $_POST['update'] == $flight->id)
     {?>
-    <form action="admin_flight.php" method="post">
+    <form action="update_flight_func.php" method="post">
       <td> <?= $flight->id ?></td>
       <td> <input type="text" name="flight_number" value="<?= $flight->flight_number ?>"> </td>
       <td> <input type="text" name="departure" value="<?= $flight->departure ?>"> </td>
---
+++ update_flight_func.php
@@ -0,0 +1,50 @@
+<?php
+session_save_path("./session/");
+session_start();
+if(isset($_SESSION['isAuth']) && $_SESSION['isAdmin'])
+{
+  require_once("db.php");
+  if(isset($_POST['update_flight']))
+  {
+    $uf = false;
+    if(preg_match('/ /', $_POST['flight_number']) || $_POST['flight_number'] ===''){
+      echo "flight_number cannot contain space or be empty<br>";
+      $uf = true;
+    }
+    if(preg_match('/ /', $_POST['departure']) || $_POST['departure'] ===''){
+      echo "depature cannot contain space or be empty<br>";
+      $uf = true;
+    }
+    if(preg_match('/ /', $_POST['destination']) || $_POST['destination'] ===''){
+      echo "destination cannot contain space or be empty<br>";
+      $uf = true;
+    }
+    if($_POST['departure_date'] ===''){
+      echo "depature_date cannot be empty<br>";
+      $uf = true;
+    }
+    if($_POST['arrival_date'] ===''){
+      echo "arrival_date cannot be empty<br>";
+      $uf = true;
+    }
+    if($uf !== true)
+    {
+    $sql = "UPDATE Flight SET flight_number = ?,"
+      ." departure = ?,"
+      ." destination = ?,"
+      ." departure_date = ?,"
+      ." arrival_date = ?"
+      ." WHERE ID = ?";
+    $update_flight = $db->prepare($sql);
+    $update_flight->execute(
+      array($_POST['flight_number'],
+      $_POST['departure'],
+      $_POST['destination'],
+      $_POST['departure_date'],
+      $_POST['arrival_date'],
+      $_POST['id']));
+      header('Location:admin_flight.php');
+    }
+  }
+}
+?>
