24a0ea8e466c8fb02c552c77f190a03ffbdc6537
Yi-Shin Shih <yihshih@cs.nctu.edu.tw>
2014-04-14 21:17:54 +0800

    fix add_user.php bug

--- add_user.php
+++ add_user.php
@@ -6,12 +6,13 @@ if(isset($_SESSION['isAuth']) && $_SESSION['isAdmin'])
   <table style="width:500px">
     <tr>
       <td> account </td>
+      <td> password </td>
       <td> identity </td>
     </tr>
     <tr>
       <form action="add_user_func.php" method="post">
       <td> <input type="text" name="account"> </td>
-      <td> <input type="text" name="password"> </td>
+      <td> <input type="password" name="password"> </td>
       <td> <INPUT TYPE="checkbox" name="is_admin" value="TRUE">admin</td>
       <td> <button type="submit">新增</button> </td>
       <td> <a href="account_management.php"><button type="button">取消</button></a> </td>
